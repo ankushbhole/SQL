2a9fc3ceaf50367a11179e846ede14095bedd76a
Ankush Bhole <ankushbhole2003@gmail.com>
2024-07-18 12:03:02 +0530

    Add files via upload

---
+++ 3. Clause Statement Tutorial.sql
@@ -0,0 +1,66 @@
+# Where Clause 
+
+SELECT *
+FROM employee_salary
+WHERE first_name = 'Tom';
+
+
+SELECT *
+FROM employee_salary
+WHERE salary >= '50000';
+
+SELECT *
+FROM employee_demographics
+WHERE gender = 'female';
+
+SELECT *
+FROM employee_demographics
+WHERE gender != 'female';
+
+#here we have used ! it means not equal
+
+-- AND OR NOT are logical operator 
+
+SELECT *
+FROM employee_demographics
+WHERE age > '40'
+AND gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE age > '40'
+OR gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE age > '40'
+OR NOT gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Tom' AND age = 36) OR gender = 'male' ;
+
+-- LIKE statement 
+-- % and _
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'T%';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a_%';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date LIKE '1989%'
+
+
+
+
+
+
