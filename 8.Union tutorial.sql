ca027a5d3c82c3f1df8518547697bb8bad62e7b5
Ankush Bhole <ankushbhole2003@gmail.com>
2024-07-22 11:38:50 +0530

    Add files via upload

---
+++ 8.Union tutorial.sql
@@ -0,0 +1,54 @@
+-- UNION
+
+SELECT age, gender
+FROM employee_demographics
+UNION
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name, 'Old Man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'MALE'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'FEMALE'
+UNION
+SELECT first_name, last_name, 'Highly Paid' AS Label
+FROM employee_salary
+WHERE salary >70000
+ORDER BY first_name, last_name;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
